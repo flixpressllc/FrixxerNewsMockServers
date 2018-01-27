64786839f77c45de5dc3a0c502c1c10c879befb0
Don Denton <don@happycollision.com>
2018-01-26 21:15:57 -0600

    Version bump for bugfix where publish from Windows breaks files on Unix

---
+++ prepublish.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+const eol = require('eol')
+
+const filesToEnsureUnixEndings = ['bin/frixxer-news-mock-servers'];
+
+function convertToUnix(file) {
+  fs.readFile(file, 'utf8', (err, text) => {
+    if (err) {
+      return console.log(err)
+    }
+    const result = eol.lf(text)
+
+    fs.writeFile(file, result, 'utf8', (err) => {
+       if (err) return console.log(err)
+    })
+  })
+}
+
+filesToEnsureUnixEndings.forEach(file => convertToUnix(path.join(__dirname, file)))
