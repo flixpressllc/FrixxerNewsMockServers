1544f7b2631b82c21777daf71b06d1033fd045c4
Don Denton <don@happycollision.com>
2018-04-04 12:53:37 -0500

    cosmetic

--- prepublish.js
+++ prepublish.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const eol = require('eol')
 
-const filesToEnsureUnixEndings = ['bin/frixxer-news-mock-servers'];
+const filesToEnsureUnixEndings = ['bin/frixxer-news-mock-servers']
 
-function convertToUnix(file) {
+function convertToUnix (file) {
   fs.readFile(file, 'utf8', (err, text) => {
     if (err) {
       return console.log(err)
@@ -12,7 +12,7 @@ function convertToUnix(file) {
     const result = eol.lf(text)
 
     fs.writeFile(file, result, 'utf8', (err) => {
-       if (err) return console.log(err)
+      if (err) return console.log(err)
     })
   })
 }
